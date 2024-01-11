e346150f3a40d294dff6d91db8a6ac70e904b8ec
Ravi <pagidipala.ravi@gmail.com>
2024-01-11 20:24:13 +0530

    initial commit
    created locations html and js file

---
+++ js/locations-js.js
@@ -0,0 +1,6 @@
+function main() {
+    document.getElementById("tr1").innerHTML = 
+    "<td>1000</td><td>1297</td><td>Via Cola di Rie</td><td>00989</td><td>Roma</td><td>IT</td>";
+}
+//TODO: use AJAX to get the data from the middleware
+//http://localhost:6010/locations/
