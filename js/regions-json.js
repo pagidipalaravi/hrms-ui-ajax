9bde35e15cde0cf9ed59f6cf1c5e257209015f7e
shaiksalma652 <salmashaik.cloud@gmail.com>
2024-01-23 16:40:26 +0530

    called the data from middleware by using json
    called the data from middleware by using json

--- js/regions-json.js
+++ js/regions-json.js
@@ -2,17 +2,19 @@ function main() {
     let regionsArray = [];
     let header =
         "<tr><th>Region_ID</th><th>REGION_NAME</th></tr>";
-    $.getJSON("../json/regions.json", function(response){
+    $.getJSON("http://localhost:6010/regions/", function(response){
         regionsArray = response;
         console.log(regionsArray);
-    });
-    let rows = "";
+		
+		let rows = "";
 
-    for(let i=0;i<regionsArray.length;i++){
-        let regionId = locationsArray[i].regionId;
-        let regionName = locationsArray[i].regionName;
-        let row = "<td>"+regionId+"</td><td>"+regionName+"</td>";
-        rows = rows + row;
-    }
-    document.getElementById("regions").innerHTML = "<table>" + header + "" + rows + "</table>"
-    }
+		for(let i=0;i<regionsArray.length;i++){
+			let regionid = regionsArray[i].regionid;
+			let regionName = regionsArray[i].regionName;
+			let row = "<tr><td>"+regionid+"</td><td>"+regionName+"</td></tr>";
+			rows = rows + row;
+		}
+		document.getElementById("regions").innerHTML = 
+		"<table>" + header + "" + rows + "</table>"
+    });
+}
