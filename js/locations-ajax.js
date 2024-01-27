4402e1fe017340477b8208aef63d7c79ad5326ba
pavangopal <pavangopal.kothuri1@gmail.com>
2024-01-27 16:16:18 +0530

    LOCATION AJAX

--- js/locations-ajax.js
+++ js/locations-ajax.js
@@ -1,7 +1,45 @@
-function main() {
-    document.getElementById("tr1").innerHTML = 
-    "<td>1000</td><td>1297</td><td>Via Cola di Rie</td><td>00989</td><td>Roma</td><td>IT</td>";
-}
-//TODO: use AJAX to get the data from the middleware
+async function main() {
 
-//http://localhost:6010/locations/
+    let locations = [];
+
+    let xhttp = new XMLHttpRequest;
+
+    xhttp.open("GET","http://localhost:6010/locations/");
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+            const text = JSON.parse(xhttp.response);
+
+            locations = text.result;
+
+            head = `<tr>
+                        <th>LOCATIONS ID</th>
+                        <th>STREET ADDRESS</th>
+                        <th>POSTAL CODE</th>
+                        <th>CITY</th>
+                        <th>STATE PROVINCE</th>
+                        <th>COUNTRY ID</th>
+                    </tr>`
+
+            data = "";
+
+            for(i=0;i<locations.length;i++){
+                data += `<tr>
+                            <td>${locations[i].locationId}</td>
+                            <td>${locations[i].streetAddress}</td>
+                            <td>${locations[i].postalCode}</td>
+                            <td>${locations[i].city}</td>
+                            <td>${locations[i].stateProvince}</td>
+                            <td>${locations[i].countryId}</td>
+                        </tr>`
+            }
+
+            t = "<table>" + head + data + "</table>";
+
+            document.getElementById("result").innerHTML = t;
+
+        }
+    }
+}
